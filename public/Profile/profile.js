c4c3993ca3c9c3f7a3b4aa68614f1180c1c7b5dd
MuhammadAbdullahSultan <abdullahsultan14@gmail.com>
2017-09-24 17:48:15 +0800

    +1
    Changing password complete!

--- public/Profile/profile.js
+++ public/Profile/profile.js
@@ -1,5 +1,5 @@
 /*global angular*/
-var app = angular.module('create', ['ngRoute']);
+var app = angular.module('profile', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     'use strict';
@@ -9,31 +9,40 @@ app.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
-app.controller('profileCtrl', ['$scope', '$rootScope', '$firebaseObject', 'Auth', 'toaster', function ($scope, $rootScope, $firebaseObject, Auth, toaster) {
+app.controller('profileCtrl', ['$scope', '$rootScope', '$firebaseObject', 'toaster', 'Auth', function ($scope, $rootScope, $firebaseObject, toaster, Auth) {
     
     // Change user password
     $scope.changePassword = function () {
         
         // check for password match
-        if ($scope.newPassword != $scope.repeatNewPassword) {
+        if ($scope.newPassword != $scope.newPasswordrepeat) {
             toaster.pop({type: 'error', title: "Password mismatch", body: 'Both passwords must match'});
             // returns outside if no match i.e. will not change password unless function called again
             return;
         }
         
-        var userData = Auth.EmailAuthProvider.credential($scope.email, $scope.currentPassword);
-        firebase.auth().currentUser.reauthenticate(credentials)
+        if($scope.newPassword.length < 6 || $scope.newPasswordrepeat.length < 6) {
+            
+            toaster.pop({type: 'error', title: "Error", body: "Password should be at least 6 characters"});
+            return;
+        }
+        
+        
+        var userData = firebase.auth.EmailAuthProvider.credential($scope.email, $scope.currentPass);
+        firebase.auth().currentUser.reauthenticateWithCredential(userData)
             .then(function() {
                 Auth.$updatePassword($scope.newPassword)
                     .then(function(){
                     toaster.pop({type: 'success', title: "Success", body: 'Password changed'});
+                    $scope.currentPass = "";
+                    $scope.newPassword = "";
+                    $scope.newPasswordrepeat = "";
                 });
         })
             // Any error will log an error
             .catch(function(error) {
-                console.error(error);
-            // Create a toaster error message
-            toaster.pop({type: 'error', title: "Error", body: error.message});
+            
+            toaster.pop({type: 'error', title: "Error", body: error});
             $scope.$apply();
         });
     };
